903f61b61b3136cf11dc8fa0eafdce7c0ece4ec9
Jeffery Moulton <jeff@jefferymoulton.com>
2020-01-07 19:04:02 -0700

    Initial commit.

---
+++ src/main/java/com/writerxl/api/WriterxlApiApplication.java
@@ -0,0 +1,13 @@
+package com.writerxl.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriterxlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WriterxlApiApplication.class, args);
+    }
+
+}
